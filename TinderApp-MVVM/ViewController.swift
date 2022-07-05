a5864e034eb08a3c9b8a0a644a38f604dac19cc4
mikekondo <foad5969@gmail.com>
2022-07-05 15:41:00 +0900

    no storyboard設定

---
+++ TinderApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderApp-MVVM
+//
+//  Created by 近藤米功 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
