43e9cd37d8df9c7165cfa31539c3f9edc5d25da6
mikekondo <foad5969@gmail.com>
2022-07-05 17:01:22 +0900

    ボタン追記

---
+++ TinderApp-MVVM/Extensions/UIImageExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImageExtension.swift
+//  TinderApp-MVVM
+//
+//  Created by 近藤米功 on 2022/07/05.
+//
+
+import UIKit
+extension UIImage {
+
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+
+        let resizeSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+
+        UIGraphicsBeginImageContextWithOptions(resizeSize, false, 0.0)
+        draw(in: CGRect(origin: .zero, size: resizeSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return resizedImage
+    }
+
+}
